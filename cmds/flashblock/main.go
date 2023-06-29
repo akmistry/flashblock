b0182cac75b3b484e2bbbc3f66fb26ddf413f3b6
Anand K. Mistry <anand@mistry.ninja>
2023-06-29 00:36:38 +0000

    Start skeleton driver binary

--- cmds/flashblock/main.go
+++ cmds/flashblock/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
+
+	_ "github.com/akmistry/go-nbd"
+)
+
+const (
+	blockSize = 4096
+
+	defaultEraseBlockSize = 1024 * 1024
+)
+
+var (
+	deviceFlag = flag.String("device", "/dev/nbd0", "Path to /deb/nbdX device.")
+
+	eraseBlockSizeFlag = flag.Uint64(
+		"erase-block-size", defaultEraseBlockSize, "Erase block size (bytes)")
+)
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 1 {
+		log.Fatal("Backing file MUST be specified")
+	}
+
+	f, err := os.OpenFile(args[0], os.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("Error opening backing file: %v", err)
+	}
+	defer f.Close()
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/akmistry/flashblock
 
 go 1.20
+
+require github.com/akmistry/go-nbd v0.0.0-20230218100447-0634a7eefbe1
+
+require (
+	github.com/akmistry/go-util v0.0.0-20230218095840-89c981669cf8 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/genetlink v1.3.1 // indirect
+	github.com/mdlayher/netlink v1.7.1 // indirect
+	github.com/mdlayher/socket v0.4.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)

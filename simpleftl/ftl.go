f13e5bfe5ade33d58bf142729c1ae16ac458cc7f
Anand K. Mistry <anand@mistry.ninja>
2023-07-05 08:52:27 +0000

    simpleftl: Rename "block" to "sector"

--- simpleftl/ftl.go
+++ simpleftl/ftl.go
@@ -20,17 +20,17 @@ var (
 )
 
 const (
-	blockOverhead = 4 * 8
+	sectorOverhead = 4 * 8
 )
 
 type eraseBlockInfo struct {
 	f     *Ftl
 	index int64
 
-	// Vector of (block, offset+1) pairs. A negative offset indicates
-	// the block has been removed. A zero offset indicates an unused entry.
-	activeBlocks       int
-	activeBlocksOffset int64
+	// Vector of (sector, offset+1) pairs. A negative offset indicates
+	// the sector has been removed. A zero offset indicates an unused entry.
+	activeSectors       int
+	activeSectorsOffset int64
 
 	nextWrite int64
 
@@ -46,51 +46,51 @@ func (i *eraseBlockInfo) erase() {
 	i.nextWrite = 0
 	i.eraseCount++
 
-	i.activeBlocks = 0
-	i.activeBlocksOffset = 0
+	i.activeSectors = 0
+	i.activeSectorsOffset = 0
 
 	i.f.chip.EraseBlock(i.index)
 }
 
-func (i *eraseBlockInfo) appendBlock(block, offset int64) {
+func (i *eraseBlockInfo) appendSector(sector, offset int64) {
 	if offset >= i.f.eraseBlockCapacity {
 		log.Printf("ERROR: offset %d >= block size %d", offset, i.f.eraseBlockCapacity)
 	}
 
 	var buf [2 * 8]byte
-	le.PutUint64(buf[0:], uint64(block))
+	le.PutUint64(buf[0:], uint64(sector))
 	le.PutUint64(buf[8:], uint64(offset+1))
-	_, err := i.f.chip.WriteAtBlock(i.index, buf[:], i.f.eraseBlockCapacity+i.activeBlocksOffset)
+	_, err := i.f.chip.WriteAtBlock(i.index, buf[:], i.f.eraseBlockCapacity+i.activeSectorsOffset)
 	if err != nil {
-		log.Printf("ERROR: write of active blocks failed: %v", err)
+		log.Printf("ERROR: write of active sectors failed: %v", err)
 	}
-	i.activeBlocksOffset += int64(len(buf))
+	i.activeSectorsOffset += int64(len(buf))
 
-	i.activeBlocks++
+	i.activeSectors++
 }
 
-func (i *eraseBlockInfo) removeBlock(block int64) {
+func (i *eraseBlockInfo) removeSector(sector int64) {
 	/*
-		if int64(len(i.activeBlockMap)) >= (i.f.blocksPerEraseBlock * 4) {
+		if int64(len(i.activeBlockMap)) >= (i.f.sectorsPerEraseBlock * 4) {
 			log.Printf("ERROR: blockmap entries %d > expected %d",
-				len(i.activeBlockMap), (i.f.blocksPerEraseBlock * 4))
+				len(i.activeBlockMap), (i.f.sectorsPerEraseBlock * 4))
 		}
 	*/
 
 	var buf [2 * 8]byte
 	offset := int64(-1)
-	le.PutUint64(buf[0:], uint64(block))
+	le.PutUint64(buf[0:], uint64(sector))
 	le.PutUint64(buf[8:], uint64(offset))
-	_, err := i.f.chip.WriteAtBlock(i.index, buf[:], i.f.eraseBlockCapacity+i.activeBlocksOffset)
+	_, err := i.f.chip.WriteAtBlock(i.index, buf[:], i.f.eraseBlockCapacity+i.activeSectorsOffset)
 	if err != nil {
-		log.Printf("ERROR: write of active blocks failed: %v", err)
+		log.Printf("ERROR: write of active sectors failed: %v", err)
 	}
-	i.activeBlocksOffset += int64(len(buf))
+	i.activeSectorsOffset += int64(len(buf))
 
-	i.activeBlocks--
+	i.activeSectors--
 
-	if i.activeBlocks < 0 {
-		log.Printf("ERROR: invalid activeBlocks: %d", i.activeBlocks)
+	if i.activeSectors < 0 {
+		log.Printf("ERROR: invalid activeSectors: %d", i.activeSectors)
 	}
 }
 
@@ -99,56 +99,56 @@ func (i *eraseBlockInfo) readActiveBlocks() []int64 {
 	buf := make([]byte, bufSize)
 	_, err := i.f.chip.ReadAtBlock(i.index, buf, i.f.eraseBlockCapacity)
 	if err != nil {
-		log.Printf("ERROR: read of active blocks failed: %v", err)
+		log.Printf("ERROR: read of active sectors failed: %v", err)
 	}
 
 	var bs []int64
 	for i := 0; i < int(bufSize); i += 16 {
-		block := int64(le.Uint64(buf[i:]))
+		sector := int64(le.Uint64(buf[i:]))
 		offset := int64(le.Uint64(buf[i+8:]))
-		bs = append(bs, block, offset)
+		bs = append(bs, sector, offset)
 	}
 	return bs
 }
 
 func (i *eraseBlockInfo) readActiveBlocksMap() (map[int64]int64, int) {
-	activeBlocksMap := make(map[int64]int64)
-	activeBlocks := i.readActiveBlocks()
-	blockEntries := 0
-	for i := 0; i < len(activeBlocks); i += 2 {
-		block := activeBlocks[i]
-		offset := activeBlocks[i+1]
+	activeSectorsMap := make(map[int64]int64)
+	activeSectors := i.readActiveBlocks()
+	sectorEntries := 0
+	for i := 0; i < len(activeSectors); i += 2 {
+		sector := activeSectors[i]
+		offset := activeSectors[i+1]
 		if offset == 0 {
 			// End of entries
 			break
 		}
-		blockEntries++
+		sectorEntries++
 		if offset > 0 {
-			activeBlocksMap[block] = offset - 1
+			activeSectorsMap[sector] = offset - 1
 		} else {
-			delete(activeBlocksMap, block)
+			delete(activeSectorsMap, sector)
 		}
 	}
-	return activeBlocksMap, blockEntries
+	return activeSectorsMap, sectorEntries
 }
 
 type Ftl struct {
 	// Size of a block/sector/page
-	blockSize int64
-	// Number of blocks
+	sectorSize int64
+	// Number of sectors
 	numBlocks int64
 
 	// Number of "erase blocks"
 	numEraseBlocks int64
-	// Number of blocks per "erase block". Not necessarily
-	// eraseBlockSize/blockSize.
-	blocksPerEraseBlock int64
+	// Number of sectors per "erase block". Not necessarily
+	// eraseBlockSize/sectorSize.
+	sectorsPerEraseBlock int64
 	// Number of bytes a "erase block" can hold. May be less than eraseBlockSize.
 	eraseBlockCapacity int64
 
 	chip *flashblock.Chip
 
-	blockMap    []int64
+	sectorMap   []int64
 	eraseBlocks []*eraseBlockInfo
 
 	currentWriteEraseBlock *eraseBlockInfo
@@ -157,34 +157,34 @@ type Ftl struct {
 	lock sync.Mutex
 }
 
-func New(blockSize int64, chip *flashblock.Chip) *Ftl {
-	if blockSize <= 0 {
-		panic("blockSize MUST be positive")
-	} else if bits.OnesCount64(uint64(blockSize)) != 1 {
-		panic("blockSize MUST be a power-of-2")
-	} else if chip.EraseBlockSize()%blockSize != 0 {
-		panic("erase block size MUST be a multiple of blockSize")
+func New(sectorSize int64, chip *flashblock.Chip) *Ftl {
+	if sectorSize <= 0 {
+		panic("sectorSize MUST be positive")
+	} else if bits.OnesCount64(uint64(sectorSize)) != 1 {
+		panic("sectorSize MUST be a power-of-2")
+	} else if chip.EraseBlockSize()%sectorSize != 0 {
+		panic("erase block size MUST be a multiple of sectorSize")
 	}
 
-	blocksPerEraseBlock := chip.EraseBlockSize() / blockSize
-	eraseBlockOverhead := blocksPerEraseBlock * blockOverhead
-	overheadBlocks := (eraseBlockOverhead + blockSize - 1) / blockSize
-	blocksPerEraseBlock -= overheadBlocks
+	sectorsPerEraseBlock := chip.EraseBlockSize() / sectorSize
+	eraseBlockOverhead := sectorsPerEraseBlock * sectorOverhead
+	overheadBlocks := (eraseBlockOverhead + sectorSize - 1) / sectorSize
+	sectorsPerEraseBlock -= overheadBlocks
 
-	numBlocks := blocksPerEraseBlock * chip.EraseBlockCount()
+	numBlocks := sectorsPerEraseBlock * chip.EraseBlockCount()
 	f := &Ftl{
-		blockSize:           blockSize,
-		numBlocks:           numBlocks,
-		numEraseBlocks:      chip.EraseBlockCount(),
-		blocksPerEraseBlock: blocksPerEraseBlock,
-		eraseBlockCapacity:  blocksPerEraseBlock * blockSize,
+		sectorSize:           sectorSize,
+		numBlocks:            numBlocks,
+		numEraseBlocks:       chip.EraseBlockCount(),
+		sectorsPerEraseBlock: sectorsPerEraseBlock,
+		eraseBlockCapacity:   sectorsPerEraseBlock * sectorSize,
 
 		chip:        chip,
-		blockMap:    make([]int64, numBlocks),
+		sectorMap:   make([]int64, numBlocks),
 		eraseBlocks: make([]*eraseBlockInfo, chip.EraseBlockCount()),
 	}
-	for i := range f.blockMap {
-		f.blockMap[i] = -1
+	for i := range f.sectorMap {
+		f.sectorMap[i] = -1
 	}
 	for i := range f.eraseBlocks {
 		ebi := &eraseBlockInfo{
@@ -193,28 +193,28 @@ func New(blockSize int64, chip *flashblock.Chip) *Ftl {
 		}
 		f.eraseBlocks[i] = ebi
 
-		activeBlocks, blockEntries := ebi.readActiveBlocksMap()
-		if len(activeBlocks) == 0 {
+		activeSectors, blockEntries := ebi.readActiveBlocksMap()
+		if len(activeSectors) == 0 {
 			if blockEntries > 0 {
 				// Should have already been erased.
 				ebi.erase()
 			}
 			f.freeBlocks.PushBack(ebi)
 		} else {
-			ebi.activeBlocks = len(activeBlocks)
-			ebi.activeBlocksOffset = int64(blockEntries) * 16
+			ebi.activeSectors = len(activeSectors)
+			ebi.activeSectorsOffset = int64(blockEntries) * 16
 
 			log.Printf("Mapping used erase block %d, entries: %d", i, blockEntries)
 			log.Printf("Active blocks: %d, active block offset: %d",
-				ebi.activeBlocks, ebi.activeBlocksOffset)
-			for block, offset := range activeBlocks {
-				writeBlockIndex := f.eraseBlockOffsetToBlockIndex(int64(i), offset)
-				f.blockMap[block] = writeBlockIndex
+				ebi.activeSectors, ebi.activeSectorsOffset)
+			for sector, offset := range activeSectors {
+				writeSectorIndex := f.eraseBlockOffsetToSectorIndex(int64(i), offset)
+				f.sectorMap[sector] = writeSectorIndex
 			}
 		}
 	}
-	log.Printf("block map entries: %d, erase blocks: %d, blocksPerEraseBlock: %d",
-		len(f.blockMap), len(f.eraseBlocks), f.blocksPerEraseBlock)
+	log.Printf("sector map entries: %d, erase blocks: %d, sectorsPerEraseBlock: %d",
+		len(f.sectorMap), len(f.eraseBlocks), f.sectorsPerEraseBlock)
 
 	go f.dumpHist()
 
@@ -222,7 +222,7 @@ func New(blockSize int64, chip *flashblock.Chip) *Ftl {
 }
 
 func (f *Ftl) Size() int64 {
-	return f.blockSize * int64(len(f.blockMap))
+	return f.sectorSize * int64(len(f.sectorMap))
 }
 
 func (f *Ftl) dumpHist() {
@@ -260,27 +260,27 @@ func (f *Ftl) generateEraseCountHist() []int {
 func (f *Ftl) generateUtilHist() []int {
 	hist := make([]int, 17)
 	for _, b := range f.eraseBlocks {
-		if b.activeBlocks == 0 {
+		if b.activeSectors == 0 {
 			continue
 		}
 
-		hist[b.activeBlocks/16]++
+		hist[b.activeSectors/16]++
 	}
 	return hist
 }
 
-func (f *Ftl) eraseBlockIndexOffset(block int64) (i int64, off int64) {
-	i = block / f.blocksPerEraseBlock
-	off = (block % f.blocksPerEraseBlock) * f.blockSize
+func (f *Ftl) eraseBlockIndexOffset(sector int64) (i int64, off int64) {
+	i = sector / f.sectorsPerEraseBlock
+	off = (sector % f.sectorsPerEraseBlock) * f.sectorSize
 	return
 }
 
-func (f *Ftl) eraseBlockOffsetToBlockIndex(eb, off int64) int64 {
-	return eb*f.blocksPerEraseBlock + (off / f.blockSize)
+func (f *Ftl) eraseBlockOffsetToSectorIndex(eb, off int64) int64 {
+	return eb*f.sectorsPerEraseBlock + (off / f.sectorSize)
 }
 
-func (f *Ftl) getCurrentBlock(block int64) (*eraseBlockInfo, int64) {
-	blockIndex := f.blockMap[block]
+func (f *Ftl) getCurrentSector(sector int64) (*eraseBlockInfo, int64) {
+	blockIndex := f.sectorMap[sector]
 	if blockIndex < 0 {
 		return nil, 0
 	}
@@ -288,14 +288,14 @@ func (f *Ftl) getCurrentBlock(block int64) (*eraseBlockInfo, int64) {
 	return f.eraseBlocks[ebIndex], ebOffset
 }
 
-func (f *Ftl) readBlock(p []byte, block int64) error {
-	if len(p) != int(f.blockSize) {
-		log.Printf("ERROR: read len(p) %d != blockSize", len(p))
+func (f *Ftl) readSector(p []byte, sector int64) error {
+	if len(p) != int(f.sectorSize) {
+		log.Printf("ERROR: read len(p) %d != sectorSize", len(p))
 	}
 
-	ebi, off := f.getCurrentBlock(block)
+	ebi, off := f.getCurrentSector(sector)
 	if ebi == nil {
-		// Unallocated block, fill zeros
+		// Unallocated sector, fill zeros
 		for i := range p {
 			p[i] = 0
 		}
@@ -321,18 +321,18 @@ func (f *Ftl) fetchEmptyEraseBlock() *eraseBlockInfo {
 		if b == ebi {
 			continue
 		}
-		if gcEbi == nil || b.activeBlocks < lastContentLen {
+		if gcEbi == nil || b.activeSectors < lastContentLen {
 			gcEbi = b
-			lastContentLen = b.activeBlocks
+			lastContentLen = b.activeSectors
 		}
 	}
 
 	log.Printf("GCing erase block %d, utilisation %d/%d",
-		gcEbi.index, lastContentLen, f.blocksPerEraseBlock)
+		gcEbi.index, lastContentLen, f.sectorsPerEraseBlock)
 
-	buf := make([]byte, f.blockSize)
-	activeBlocks, _ := gcEbi.readActiveBlocksMap()
-	for block, offset := range activeBlocks {
+	buf := make([]byte, f.sectorSize)
+	activeSectors, _ := gcEbi.readActiveBlocksMap()
+	for sector, offset := range activeSectors {
 		_, err := f.chip.ReadAtBlock(gcEbi.index, buf, int64(offset))
 		if err != nil {
 			log.Printf("ERROR: ReadAtBlock(%d, %d, %d) error: %v",
@@ -340,10 +340,10 @@ func (f *Ftl) fetchEmptyEraseBlock() *eraseBlockInfo {
 			continue
 		}
 
-		err = f.writeBlock(buf, block, ebi.index)
+		err = f.writeSector(buf, sector, ebi.index)
 		if err != nil {
-			log.Printf("ERROR: writeBlock(%d, %d, %d) error: %v",
-				len(buf), block, ebi.index, err)
+			log.Printf("ERROR: writeSector(%d, %d, %d) error: %v",
+				len(buf), sector, ebi.index, err)
 			continue
 		}
 	}
@@ -363,7 +363,7 @@ func (f *Ftl) fetchWriteBlock() *eraseBlockInfo {
 		}
 
 		//log.Printf("Filled erase block %d: utilisation %d/%d",
-		//	eb.index, len(eb.contents), f.chip.EraseBlockSize()/f.blockSize)
+		//	eb.index, len(eb.contents), f.chip.EraseBlockSize()/f.sectorSize)
 		eb = nil
 		f.currentWriteEraseBlock = nil
 	}
@@ -380,19 +380,19 @@ func (f *Ftl) fetchWriteBlock() *eraseBlockInfo {
 }
 
 func (f *Ftl) freeEraseBlockIfEmpty(ebi *eraseBlockInfo) {
-	if ebi.activeBlocks > 0 {
+	if ebi.activeSectors > 0 {
 		return
 	}
 
 	if ebi == f.currentWriteEraseBlock {
 		log.Printf("Avoid erasing current erase block %d with usage %d",
-			ebi.index, ebi.activeBlocks)
+			ebi.index, ebi.activeSectors)
 		return
 	}
 	/*
 		if !ebi.full() {
 			log.Printf("Avoid erasing non-full erase block %d with usage %d",
-				ebi.index, ebi.activeBlocks)
+				ebi.index, ebi.activeSectors)
 			return
 		}
 	*/
@@ -403,30 +403,30 @@ func (f *Ftl) freeEraseBlockIfEmpty(ebi *eraseBlockInfo) {
 	//	ebi.index, f.freeBlocks.Len())
 }
 
-func (f *Ftl) writeBlock(p []byte, block, eraseBlock int64) error {
-	if len(p) != int(f.blockSize) {
-		log.Printf("ERROR: write len(p) %d != blockSize", len(p))
+func (f *Ftl) writeSector(p []byte, sector, eraseBlock int64) error {
+	if len(p) != int(f.sectorSize) {
+		log.Printf("ERROR: write len(p) %d != sectorSize", len(p))
 	}
 
 	ebi := f.eraseBlocks[eraseBlock]
 	writeOffset := ebi.nextWrite
-	_, err := f.chip.WriteAtBlock(eraseBlock, p[:f.blockSize], writeOffset)
+	_, err := f.chip.WriteAtBlock(eraseBlock, p[:f.sectorSize], writeOffset)
 	if err != nil {
 		return err
 	}
-	ebi.appendBlock(block, writeOffset)
-	ebi.nextWrite += f.blockSize
+	ebi.appendSector(sector, writeOffset)
+	ebi.nextWrite += f.sectorSize
 
-	writeBlockIndex := f.eraseBlockOffsetToBlockIndex(eraseBlock, writeOffset)
-	f.blockMap[block] = writeBlockIndex
+	writeSectorIndex := f.eraseBlockOffsetToSectorIndex(eraseBlock, writeOffset)
+	f.sectorMap[sector] = writeSectorIndex
 
 	return nil
 }
 
 func (f *Ftl) ReadAt(p []byte, off int64) (int, error) {
-	if off%f.blockSize != 0 {
+	if off%f.sectorSize != 0 {
 		return 0, errUnalignedOffset
-	} else if int64(len(p))%f.blockSize != 0 {
+	} else if int64(len(p))%f.sectorSize != 0 {
 		return 0, errUnalignedLength
 	}
 
@@ -434,9 +434,9 @@ func (f *Ftl) ReadAt(p []byte, off int64) (int, error) {
 	defer f.lock.Unlock()
 
 	n := 0
-	for ; n < len(p); n += int(f.blockSize) {
-		block := (off + int64(n)) / f.blockSize
-		err := f.readBlock(p[n:n+int(f.blockSize)], block)
+	for ; n < len(p); n += int(f.sectorSize) {
+		sector := (off + int64(n)) / f.sectorSize
+		err := f.readSector(p[n:n+int(f.sectorSize)], sector)
 		if err != nil {
 			return n, err
 		}
@@ -446,9 +446,9 @@ func (f *Ftl) ReadAt(p []byte, off int64) (int, error) {
 }
 
 func (f *Ftl) WriteAt(p []byte, off int64) (int, error) {
-	if off%f.blockSize != 0 {
+	if off%f.sectorSize != 0 {
 		return 0, errUnalignedOffset
-	} else if int64(len(p))%f.blockSize != 0 {
+	} else if int64(len(p))%f.sectorSize != 0 {
 		return 0, errUnalignedLength
 	}
 
@@ -456,16 +456,16 @@ func (f *Ftl) WriteAt(p []byte, off int64) (int, error) {
 	defer f.lock.Unlock()
 
 	n := 0
-	for ; n < len(p); n += int(f.blockSize) {
-		block := (off + int64(n)) / f.blockSize
-		ebi, _ := f.getCurrentBlock(block)
+	for ; n < len(p); n += int(f.sectorSize) {
+		sector := (off + int64(n)) / f.sectorSize
+		ebi, _ := f.getCurrentSector(sector)
 		if ebi != nil {
-			ebi.removeBlock(block)
+			ebi.removeSector(sector)
 			f.freeEraseBlockIfEmpty(ebi)
 		}
 		ebi = f.fetchWriteBlock()
 
-		err := f.writeBlock(p[n:n+int(f.blockSize)], block, ebi.index)
+		err := f.writeSector(p[n:n+int(f.sectorSize)], sector, ebi.index)
 		if err != nil {
 			return n, err
 		}
@@ -477,9 +477,9 @@ func (f *Ftl) WriteAt(p []byte, off int64) (int, error) {
 func (f *Ftl) Trim(off int64, length uint32) error {
 	log.Printf("Trim(off = %d, len = %d)", off, length)
 
-	if off%f.blockSize != 0 {
+	if off%f.sectorSize != 0 {
 		return errUnalignedOffset
-	} else if int64(length)%f.blockSize != 0 {
+	} else if int64(length)%f.sectorSize != 0 {
 		return errUnalignedLength
 	}
 
@@ -487,16 +487,16 @@ func (f *Ftl) Trim(off int64, length uint32) error {
 	defer f.lock.Unlock()
 
 	for length > 0 {
-		block := off / f.blockSize
-		ebi, _ := f.getCurrentBlock(block)
+		sector := off / f.sectorSize
+		ebi, _ := f.getCurrentSector(sector)
 		if ebi != nil {
-			ebi.removeBlock(block)
+			ebi.removeSector(sector)
 			f.freeEraseBlockIfEmpty(ebi)
 		}
-		f.blockMap[block] = -1
+		f.sectorMap[sector] = -1
 
-		length -= uint32(f.blockSize)
-		off += f.blockSize
+		length -= uint32(f.sectorSize)
+		off += f.sectorSize
 	}
 
 	return nil
